5e8a06b93b19149ea912fee294d9a50c4df0cf33
Joel West <emailjoelwest@gmail.com>
2022-01-16 22:44:17 +1100

    Think it's finished/working - do more testing tomorrow

--- hard.py
+++ hard.py
@@ -6,8 +6,6 @@ from posixpath import split
 import re
 from pyparsing import nestedExpr
 
-#--------------------------------------------------------------------------------------------
-
 # Determines if this string is a course code
 # Returns a bool
 def is_course_code(word):
@@ -48,37 +46,31 @@ def wanted_word(word):
 # Cleans the word list by making everything uppercase, pruning
 # unwanted words and fixing bad course codes
 def clean_word_list(word_list):
-    # Is there a better way of doing this without a gross double loop?
-    # Problem is, I'm modifying this list within the loop and would
-    # have issues if I just got rid of the outermost while loop?
-    # Probably a better way... Had to do this same grossness later on as well
-    clean = False
-    while not clean:
-        clean = True
-        for i, item in enumerate(word_list):
-            if type(item) is str:
-                # It's a string, clean it up.
-                # For each word, only keep alphanumeric characters
-                word_list[i] = ''.join(filter(str.isalnum, word_list[i]))
-                word_list[i] = word_list[i].upper()
+    cleaned_list = []
+    for item in word_list:
+        if type(item) is str:
+            # It's a string, clean it up.
+            # For each word, only keep alphanumeric characters
+            item = ''.join(filter(str.isalnum, item))
+            item = item.upper()
 
-                # If it's just a course code written badly, ie without the
-                # faculty at the start like COMP or MATH, assume they meant
-                # to put COMP
-                if is_bad_course_code(word_list[i]):
-                    word_list[i] = 'COMP' + word_list[i]
+            # If it's just a course code written badly, ie without the
+            # faculty at the start like COMP or MATH, assume they meant
+            # to put COMP
+            if is_bad_course_code(item):
+                item = 'COMP' + item
 
-                # If it's the word prerequisite then remove it and skip
-                if not wanted_word(word_list[i]):
-                    # Remove the item if it's prereq
-                    word_list = word_list[:i] + word_list[i + 1:]
-                    clean = False
-                    break
-            else:
-                # It's a list, go deeper
-                word_list[i] = clean_word_list(item)
+            # If it's the word prerequisite then remove it and skip
+            if wanted_word(item):
+                # Remove the item if it's prereq
+                cleaned_list.append(item)
+        elif type(item) is list:
+            # It's a list, go deeper
+            cleaned_list.append(clean_word_list(item))
+        else:
+            print(f"There is a problem, item = {item} is neither a list nor a string")
     
-    return word_list
+    return cleaned_list
 
 # Takes in a target course, gets the conditions in conditions.json,
 # deconstructs it into individual words and returns a cleaned
@@ -124,6 +116,7 @@ def contains_list(requirements):
         if type(requirement) is list:
             return True
 
+# Takes in a list of UOC requirements and returns a bool depending on whether the requirements are satisfied
 def is_uoc_satisfied(courses_done, requirement):
     # There are 4 cases
     # - Simple min UOC requirement, e.g. ['102', 'UNITS']
@@ -144,20 +137,23 @@ def is_uoc_satisfied(courses_done, requirement):
     if len(requirement) == 2:
         # Simple min UOC requirement, e.g. ['102', 'UNITS']
         uoc_done = 6 * len(courses_done)
-    elif faculty != None:
+    elif faculty != None and requirement.count('LEVEL') == 0:
         # Min UOC requirement in faculty e.g. ['36', 'UNITS', 'COMP']
         for course_done in courses_done:
             if course_done[:4] == faculty:
                 uoc_done += 6
-    elif faculty != None and requirement.contains('LEVEL'):
+    elif faculty != None and requirement.count('LEVEL') != 0:
         # Min UOC requirement in faculty with level x, e.g. ['18', 'UNITS', 'LEVEL', '2', 'COMP']
         level_index = requirement.index('LEVEL')
         
         # Assume the item after the word level is the level number we want
-        level_needed = int(requirement[level_index + 1])
-        for course_done in courses_done:
-            if course_done[:4] == faculty and int(course_done[4]) == level_needed:
-                uoc_done += 6
+        try:
+            level_needed = int(requirement[level_index + 1])
+            for course_done in courses_done:
+                if course_done[:4] == faculty and int(course_done[4]) == level_needed:
+                    uoc_done += 6
+        except:
+            print(f"Something went wrong when checking the level_needed. Expected {requirement[level_index + 1]} to be an int")
     elif contains_list(requirement):
         # Min UOC requirement from a list of courses, e.g. ['12', 'UNITS', ['COMP6443', 'COMP6843', 'COMP6445', 'COMP6845', 'COMP6447']]
         possible_courses = requirement[-1]
@@ -172,92 +168,130 @@ def is_uoc_satisfied(courses_done, requirement):
     
     return uoc_done >= uoc_required
 
-def satisfies_requirements(courses_done, requirements):    
-    if len(requirements) == 0:
-        return True
+# Helper to decide if this index is the last index
+# of a UOC requirement
+def end_of_uoc_requirement(index, requirements):
+    return index + 1 < len(requirements) and requirements[index + 1] != 'OR' and requirements[index + 1] != 'AND'
+
+# Goes through each item in requirements, makes a new list that corresponds to the requirements
+# list. If item is course code -> bool on whether student has done it. If item is UOC requirement
+# -> bool on whether student satisfied UOC. If it's either 'AND' or 'OR', add them into the list
+# for later. If it's a nested list, sort that out first and it becomes a bool depending on
+# the contents of the list.
+def create_boolean_list(courses_done, requirements):    
+    # Requirements looks something along the lines of this for example. This is COMP4601:
+    # [['COMP2511', 'OR', 'COMP2911'], 'AND', '24', 'UNITS']
+    # Nested list indicates brackets
     
+    # Create a new list and assume that everything hasn't been checked.
+    # Each item in boolean_list will either be a bool (if the corresponding item in requirements
+    # is either a course code or a UOC requirement of some sorts) or 'OR' or 'AND' (again, depending on what was
+    # originally in requirements). Must also account for nested lists.
+    boolean_list = []
     everything_checked = False
     while not everything_checked:
         everything_checked = True
+        # For each item in requirements, check what category it's in and act accordingly
         for i, item in enumerate(requirements):
             if type(item) is str:
                 if item == 'OR' or item == 'AND':
                     # Don't need to do anything to these for now
-                    continue
+                    boolean_list.append(item)
                 elif is_course_code(item):
                     # Decide whether this item should be True or False.
                     # True -> student has done this course.
                     # False -> student hasn't done this course.
-                    requirements[i] = (item in courses_done)
+                    boolean_list.append(item in courses_done)
                 else:
                     # Must be some UOC requirement.
-                    # First thing is always the number of UOC, at least in examples given
-                    #uoc_required = int(item)
-
                     # Need to figure out how long this UOC requirement is
-                    lo = i
-                    hi = lo
-                    while hi + 1 < len(requirements) and requirements[hi + 1] != 'OR' and requirements[hi + 1] != 'AND':
+                    lo = hi = i
+                    while end_of_uoc_requirement(hi, requirements):
                         hi += 1
                     
                     # Isolate just the UOC requirement bit to simplify
                     uoc_requirement = requirements[lo:hi + 1]
 
                     # Figure out if this should be True or False
-                    requirements[hi] = is_uoc_satisfied(courses_done, uoc_requirement)
+                    boolean_list.append(is_uoc_satisfied(courses_done, uoc_requirement))
 
                     # Get rid of the wacky bits and break so that the enumeration doesn't break
-                    requirements = requirements[:lo] + requirements[hi:]
+                    requirements = requirements[hi + 1:]
                     everything_checked = False
                     break
-            elif type(item) is bool:
-                # Do nothing
-                continue
-            else:
+            elif type(item) is list:
                 # It's a list, go deeper
-                requirements[i] = satisfies_requirements(courses_done, item)
+                boolean_list.append(satisfies_requirements(courses_done, item))
+            else:
+                print(f"There is a problem since {item} is neither a string nor a list")
+    return boolean_list
+
+# Takes in requirements and turns them into bools of either True or False depending
+# on whether each requirement is satisfied. Then, after each course code/UOC requirement has been turned into bools,
+# it goes through and checks whether OVERALL the requirements are satisfied or not
+def satisfies_requirements(courses_done, requirements):
+    # If there are no requirements, must have met the requirements    
+    if len(requirements) == 0:
+        return True
     
-    # Once we've gone through the entire list, decide if everything is satisfied or not
-    is_satisfied = requirements[0]
-    for i in range(0, len(requirements)):
+    boolean_list = create_boolean_list(courses_done, requirements)
+
+    # Once we've gone through the entire list, decide if everything is satisfied or not.
+    # At this point, boolean_list looks something like [True 'AND' False 'OR' False]
+    # and now we just need to combine them into a single answer. There won't be any
+    # nested lists at this point since we've already dealt with them in the recursion of
+    # create_boolean_list
+    is_satisfied = boolean_list[0]
+    print(boolean_list)
+    print(requirements)
+    for i, requirement in enumerate(boolean_list):
         if i % 2 == 0:
             continue
         
-        # requirements[i] is either 'AND' or 'OR' since I'm only taking i to be odd
-        if requirements[i] == 'AND':
+        # requirement is either 'AND' or 'OR' since I'm only taking i to be odd
+        if requirement == 'AND':
             # Do an and with the next item
-            is_satisfied = is_satisfied and requirements[i + 1]
-        elif requirements[i] == 'OR':
+            is_satisfied = is_satisfied and boolean_list[i + 1]
+        elif requirement == 'OR':
             # Do an or with the next item
-            is_satisfied = is_satisfied or requirements[i + 1]
+            is_satisfied = is_satisfied or boolean_list[i + 1]
         else:
-            print(f"There is a problem. This item is {requirements[i]} i is {i}")
+            print(f"There is a problem. This requirement = {requirement}, i = {i}")
     
     return is_satisfied
 
-"""
-Given a list of course codes a student has taken, return true if the target_course 
-can be unlocked by them.
 
-You do not have to do any error checking on the inputs and can assume that
-the target_course always exists inside conditions.json
 
-You can assume all courses are worth 6 units of credit
-"""
+# Given a list of course codes a student has taken, return true if the target_course 
+# can be unlocked by them.
+# 
+# You do not have to do any error checking on the inputs and can assume that
+# the target_course always exists inside conditions.json
+# 
+# You can assume all courses are worth 6 units of credit
+#
+# I've also assumed course_list is formatted as a list or tuple I think would also work.
+# Assumed all courses will valid and of the format 'XYZW1234'
+#
+# Would improve by having proper exception handling, since I've just chucked prints
+# for now, and I assume that I could do all the work in satisfies_requirements and
+# create_boolean_list in a single loop, but couldn't get it to work???? Not sure why
 def is_unlocked(courses_list, target_course):
-    conditions = get_target_conditions(target_course)    
+    conditions = get_target_conditions(target_course)
 
     # Go through and check if we've satisfied each part.
     # If we are, replace it with True. If not, replace it with False.
     # This is recursive by the nature that I've set it up
     return satisfies_requirements(courses_list, conditions)
 
-#--------------------------------------------------------------------------------------------
-
+# For testing purposes
 if __name__ == '__main__':
     with open("./conditions.json") as f:
         all_conditions = json.load(f)
 
         for key in all_conditions.keys():
-            print(f"{key}: {is_unlocked(['MATH1081'], key)}")
+            print(f"{key}: {is_unlocked(['MATH1081', 'MTRN3500', 'COMP1531', 'COMP6443',
+            'COMP2521', 'COMP9444', 'COMP2511', 'COMP2041',
+            'COMP3901', 'COMP3821', 'COMP1511', 'COMP1521'], key)}")
+            print()
         f.close()
